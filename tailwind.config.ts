7c12e4234c465538f6874f7edf9b3d98a8d23e5e
Risal Walangadi <risal.walangadi@gmail.com>
2023-09-30 16:56:42 +0800

    add colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,13 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        "tomato": "hsl(4, 100%, 67%)",
+        "dark-slate-grey": "hsl(234, 29%, 20%)",
+        "charcoal-grey": "hsl(235, 18%, 26%)",
+        "grey": "hsl(231, 7%, 60%)",
+        "white": "hsl(0, 0%, 100%)"
+      }
     },
   },
   plugins: [],
